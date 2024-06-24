2361c52dbdc423ed0fa91d86fad943c157bed125
Jules Guillou <guilloujules@gmail.com>
2024-06-24 17:49:51 +0200

    docs(all): :memo: Added sphinx documentation integration
    This documentation leverages Doxygen and provides much more controle

---
+++ docs/source/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Holoflow'
+copyright = '2024, Jules Guillou'
+author = 'Jules Guillou'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'breathe',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
+]
+
+breathe_projects = {
+    "Holoflow": "../../bazel-bin/docs/xml"
+}
+
+breathe_default_project = "Holoflow"
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
